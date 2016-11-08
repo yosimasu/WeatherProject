a790e1a04b6b9bc6c32c528af17e3d20c82206b3
Masu Lin <masu.lin@104.com.tw>
2016-11-09 00:51:43 +0800

    Fetch

--- WeatherProject.js
+++ WeatherProject.js
@@ -13,36 +13,51 @@ export default class WeatherProject extends Component {
         this._handleTextChange = this._handleTextChange.bind(this);
         this.state = {
             zip: '',
-            forecast: {
-                main: 'Clouds',
-                description: 'few clouds',
-                temp: 45.7
-            }
+            forecast: null
         }
     }
 
     _handleTextChange(event) {
-        console.log(event.nativeEvent.text);
+        let zip = event.nativeEvent.text;
         this.setState({
-           zip: event.nativeEvent.text
+            zip: zip
         });
+        fetch('http://api.openweathermap.org/data/2.5/weather?q='
+            + zip + '&unit=imperial&APPID=ac72cee04a5d145a538b97fb645b20c9')
+            .then((response) => response.json())
+            .then((responseJSON) => {
+                this.setState({
+                    forecast: {
+                        main: responseJSON.weather[0].main,
+                        description: responseJSON.weather[0].description,
+                        temp: responseJSON.main.temp
+                    }
+                });
+            })
+            .catch((error) => {
+                console.warn(error);
+            });
     }
 
     render() {
+        let content = null;
+        if (this.state.forecast !== null) {
+            content = <Forecast
+                main={this.state.forecast.main}
+                description={this.state.forecast.description}
+                temp={this.state.forecast.temp}
+            />
+        }
         return (
             <View style={styles.container}>
                 <Text style={styles.welcome}>
                     You input {this.state.zip}
                 </Text>
-                <Forecast
-                    main={this.state.forecast.main}
-                    description={this.state.forecast.description}
-                    temp={this.state.forecast.temp}
-                />
                 <TextInput
                     style={styles.input}
                     onSubmitEditing={this._handleTextChange}
                 />
+                {content}
             </View>
         );
     }
